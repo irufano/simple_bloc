492a94398cd3bff8e9792df07b9dffe2444a6070
M. Irfan Hidayat <irfanhidayatms@outlook.com>
2019-10-28 17:01:21 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:simple_bloc/bloc/counter_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CounterBloc _counterBloc = CounterBloc(0);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter RxDart"),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: StreamBuilder<int>(
+              stream: _counterBloc.counterObservable,
+              builder: (context, snapshot) {
+                return Text(
+                  "${snapshot.data}",
+                  style: Theme.of(context).textTheme.display2,
+                );
+              }),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              _counterBloc.increment();
+            },
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.remove),
+            onPressed: () {
+              setState(() {
+                _counterBloc.decrement();
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _counterBloc.dispose();
+    super.dispose();
+  }
+}
